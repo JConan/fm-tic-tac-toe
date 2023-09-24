056bbe7b50672f7dd7deff196b698ffbe44f38c5
jconan <conanjahh@hotmail.com>
2023-09-24 19:48:49 +0100

    refactor Cell to have its own components

--- src/lib/Board.svelte
+++ src/lib/Board.svelte
@@ -1,29 +1,33 @@
 <script lang="ts">
     import { board } from "$stores/Board";
+    import Cell from "./Cell.svelte";
 
     $: line1 = $board.slice(0, 3);
     $: line2 = $board.slice(3, 6);
     $: line3 = $board.slice(6, 9);
 
+    let nextPlayer = board.nextPlayer() as "X" | "O";
+
     const clickCell = (index: number) => () => {
-        board.setCell(index, board.nextPlayer());
+        nextPlayer = board.nextPlayer();
+        board.setCell(index, nextPlayer);
     };
 </script>
 
 <table>
     <tr>
         {#each line1 as cellValue, index}
-            <td on:click={clickCell(index)}>{cellValue}</td>
+            <Cell on:click={clickCell(index)} {cellValue} {nextPlayer} />
         {/each}
     </tr>
     <tr>
         {#each line2 as cellValue, index}
-            <td on:click={clickCell(index + 3)}>{cellValue}</td>
+            <Cell on:click={clickCell(index + 3)} {cellValue} {nextPlayer} />
         {/each}
     </tr>
     <tr>
         {#each line3 as cellValue, index}
-            <td on:click={clickCell(index + 6)}>{cellValue}</td>
+            <Cell on:click={clickCell(index + 6)} {cellValue} {nextPlayer} />
         {/each}
     </tr>
 </table>
---
+++ src/lib/Cell.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import X from "$assets/icon-x.svg";
+    import X_outline from "$assets/icon-x-outline.svg";
+    import O from "$assets/icon-o.svg";
+    import O_outline from "$assets/icon-o-outline.svg";
+
+    export let nextPlayer: string;
+    export let cellValue: string;
+</script>
+
+<td on:click>{cellValue}</td>
