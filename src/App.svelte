d5d0346d4d0ec1fb88c767385e38740ddaf7ad1b
jconan <conanjahh@hotmail.com>
2024-07-05 13:51:53 +0200

    add listener to retrive document scroll height

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
-    import "the-new-css-reset/css/reset.css";
-    import "@fontsource-variable/outfit";
-    import favico from "/assets/favicon-32x32.png";
-    import Board from "$lib/Board.svelte";
+  import "the-new-css-reset/css/reset.css";
+  import "@fontsource-variable/outfit";
+  import favico from "/assets/favicon-32x32.png";
+  import Board from "$lib/Board.svelte";
+
+  window.addEventListener("message", (event) => {
+    if (event.data === "getHeight") {
+      const height = document.body.scrollHeight;
+      event.source?.postMessage(
+        { type: "getHeight", height },
+        event.origin as any
+      );
+    }
+  });
 </script>
 
 <svelte:head>
-    <title>Tic Tac Toe</title>
-    <link rel="icon" type="image/png" href={favico} />
+  <title>Tic Tac Toe</title>
+  <link rel="icon" type="image/png" href={favico} />
 </svelte:head>
 
 <Board />
