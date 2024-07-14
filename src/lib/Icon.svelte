8dd785b944cde1d3b6c5f1b7262020ccfb3bca7e
jconan <conanjahh@hotmail.com>
2024-07-14 18:03:18 +0200

    new Icon component to handle image assets

---
+++ src/lib/Icon.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { inlineSvg } from "@svelte-put/inline-svg";
+  import { resolveIcon, type BaseColor, type Name } from "./Icon";
+  export let name: Name;
+  export let color: BaseColor | undefined = undefined;
+  export let outlined: boolean = false;
+  export let width: number = 64;
+  export let height: number = 64;
+
+  /**
+   * Resolve asset and styling
+   */
+  $: ({ src, style, viewBox } = resolveIcon(name, outlined, color));
+</script>
+
+<svg class="icon" use:inlineSvg={src} {viewBox} {width} {height} {style} />
+
+<style>
+  svg {
+    & path {
+      stroke-width: var(--stroke-width, 0);
+      stroke: var(--stroke-color);
+      stroke-opacity: var(--stroke-opacity, 1);
+      fill: var(--fill-color);
+      fill-opacity: var(--fill-opacity, 1);
+    }
+  }
+</style>
---
+++ src/lib/Icon.ts
@@ -0,0 +1,66 @@
+export type BaseColor = "blue" | "yellow";
+export type Color = BaseColor | `${BaseColor}_outlined`;
+export interface ColorScheme {
+  fillColor: string;
+  fillOpacity?: number;
+  strokeColor?: string;
+  strokeOpacity?: number;
+  strokeWidth?: number;
+}
+
+export type Name = "X" | "O";
+export interface Asset {
+  src: string;
+  viewBox: string;
+  defaultColor: BaseColor;
+}
+
+export const assets: Record<Name, Asset> = {
+  X: {
+    src: "assets/icon-x-outline.svg",
+    viewBox: "0 0 65 65",
+    defaultColor: "blue",
+  },
+  O: {
+    src: "assets/icon-o-outline.svg",
+    viewBox: "0 0 67 67",
+    defaultColor: "yellow",
+  },
+};
+
+export const colors: Record<Color, ColorScheme> = {
+  blue: {
+    fillColor: "var(--light-blue)",
+  },
+  blue_outlined: {
+    fillColor: "var(--semi-dark-navy)",
+    strokeColor: "var(--light-blue)",
+    strokeWidth: 2,
+  },
+  yellow: {
+    fillColor: "var(--light-yellow)",
+  },
+  yellow_outlined: {
+    fillColor: "var(--dark-navy)",
+    strokeColor: "var(--light-yellow)",
+    strokeWidth: 2,
+  },
+};
+
+export function resolveIcon(name: Name, outlined: boolean, color?: BaseColor) {
+  const { src, viewBox, defaultColor } = assets[name];
+  const selectedColor =
+    colors[`${color ?? defaultColor}${outlined ? "_outlined" : ""}` as Color];
+  let style: string = "";
+  const { fillColor, strokeColor, strokeWidth, strokeOpacity, fillOpacity } =
+    selectedColor;
+
+  style = "";
+  if (fillColor) style += `--fill-color: ${fillColor};`;
+  if (fillOpacity) style += `--fill-opacity: ${fillOpacity};`;
+  if (strokeColor) style += `--stroke-color: ${strokeColor};`;
+  if (strokeOpacity) style += `--stroke-opacity: ${strokeOpacity};`;
+  if (strokeWidth) style += `--stroke-width: ${strokeWidth};`;
+
+  return { src, viewBox, style };
+}
