d203914edb5e96bbf4ee5f0ad0bf59ed57ffdaaa
jconan <conanjahh@hotmail.com>
2024-07-16 11:11:04 +0200

    reorganize base component

--- src/lib/components/base/Button.svelte
+++ src/lib/components/base/Button.svelte
@@ -1,43 +1,7 @@
 <script lang="ts">
-  import type { HTMLButtonAttributes } from "svelte/elements";
+  import { colorSchemes, type ButtonProps } from "./Button";
 
-  interface $$Props extends HTMLButtonAttributes {
-    color?: keyof typeof colorSchemes;
-    width?: number;
-    height?: number;
-    isHoverable?: boolean;
-  }
-
-  const colorSchemes = {
-    blue: {
-      background: "--light-blue",
-      shadow: "--shadow-blue",
-    },
-    yellow: {
-      background: "--light-yellow",
-      shadow: "--shadow-yellow",
-    },
-    silver: {
-      background: "--silver",
-      shadow: "--shadow-silver",
-    },
-    "light-blue": {
-      background: "--light-blue-hover",
-      shadow: "--shadow-blue",
-    },
-    "light-yellow": {
-      background: "--light-yellow-hover",
-      shadow: "--shadow-yellow",
-    },
-    "light-silver": {
-      background: "--silver-hover",
-      shadow: "--shadow-silver",
-    },
-    dark: {
-      background: "--semi-dark-navy",
-      shadow: "--shadow-dark-navy",
-    },
-  };
+  interface $$Props extends ButtonProps {}
 
   export let color: $$Props["color"] = "yellow";
   export let width: $$Props["width"] = 120;
