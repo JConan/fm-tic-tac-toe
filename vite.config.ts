81a82ac3e6320669a9a6e587368198310d5a429c
JConan <conanjahh@hotmail.com>
2024-11-08 23:36:35 +0100

    appflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
-import { defineConfig } from 'vitest/config'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import path from 'path'
+import { defineConfig } from "vitest/config";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => ({
+export default defineConfig(({ mode }) => ({
   plugins: [svelte()],
-  base:  mode === 'production' ? '/fm-tic-tac-toe/' : '/',
+  // base: mode === "production" ? "/fm-tic-tac-toe/" : "/",
+  base: "/",
   test: {
     globals: true,
     environment: "jsdom",
-    include: ['src/**/*.{test,spec}.ts'],
-    setupFiles: './vitest-setup.ts'
+    include: ["src/**/*.{test,spec}.ts"],
+    setupFiles: "./vitest-setup.ts",
   },
   resolve: {
     alias: {
-      $lib: path.resolve('./src/lib'),
-      $stores: path.resolve('./src/stores'),
-      $assets: path.resolve('./src/assets')
-    }
-  }
-}))
+      $lib: path.resolve("./src/lib"),
+      $stores: path.resolve("./src/stores"),
+      $assets: path.resolve("./src/assets"),
+    },
+  },
+}));
